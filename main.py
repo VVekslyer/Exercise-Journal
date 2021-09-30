7d121d828c860a3bc379a5c45b56cd56014c9306
VVekslyer <69658141+VVekslyer@users.noreply.github.com>
2021-09-30 10:00:31 -0700

    added fonts + navigation

--- main.py
+++ main.py
@@ -4,46 +4,99 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
+from kivy.clock import Clock
 Config.set('graphics', 'resizable', False)
 Window.size = (410, 730)
 
-
 Builder.load_string("""
-<MenuScreen>:
+<HomeScreen>:
     BoxLayout:
         orientation: 'vertical'
         MDToolbar:
-            title: 'vitaly540@gmail.com'
+            title: 'Vitaliy Vekslyer'
+            font_name: 'assets/fonts/Inter-Medium.ttf'
             left_action_items: [["account-circle"]]
             right_action_items: [["dots-vertical"]]
             md_bg_color: app.theme_cls.bg_light
             specific_text_color: (0,0,0,1)
-            elevation: 0
-        MDLabel:
-            text: 'Home'
-            halign: 'left'
-            padding_x: '12dp'
-            padding_y: '12dp'
-            adaptive_size: True 
-            text_color: (0,0,0,1)
-            font_style: 'H4'
-        Button:
-            text: 'Workout 1'
-            on_press: root.manager.current = 'settings'
-        Button:
-            text: 'Workout 2'
-            on_press: root.manager.current = 'settings'  
-        Button:
-            text: 'Workout 3'
+            elevation: 0  
 
-        MDBottomAppBar:
-            md_bg_color: 1, 1, 1, 1
+        MDBottomNavigation:
+            text_color_active: 1, 0, 0, 1
+            panel_color: 1, 1, 1, 1
 
-            MDToolbar:
-                icon: "plus"
-                type: "bottom"
-                icon_color: 0, 0, 0, 1
-                mode: "end"
+            MDBottomNavigationItem:
+                name: "screen 1"
+                text: "Home"
+                icon: 'home'     
+
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDLabel:
+                        text: 'Home'
+                        text_size: self.size
+                        valign: 'top'
+                        halign: 'left'
+                        markup: True
+                        padding_x: '24dp'
+                        padding_y: '12dp'
+                        text_color: (0,0,0,1)
+                        font_style: 'H4'
+                        font_name: 'assets/fonts/Inter-SemiBold.ttf'
+                    MDFloatingActionButtonSpeedDial:
+                        rotation_root_button: True
+                        data: app.data
+                        bg_color_root_button: (0,0,0,1)
+                        bg_color_stack_button: (0,0,0,1)
+                        bg_color_stack_label: (0,0,0,1)
+                        bg_hint_color: (0,0,0,1)
+       
+
+            MDBottomNavigationItem:
+                name: "screen 2"
+                text: "Workout"
+                icon: 'lightning-bolt'
+
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDLabel:
+                        text: 'Workout'
+                        text_size: self.size
+                        valign: 'top'
+                        halign: 'left'
+                        markup: True
+                        padding_x: '24dp'
+                        padding_y: '12dp'
+                        text_color: (0,0,0,1)
+                        font_style: 'H4'
+                        font_name: 'assets/fonts/Inter-SemiBold.ttf'
+                    MDFloatingActionButtonSpeedDial:
+                        rotation_root_button: True
+                        data: app.data
+                        bg_color_root_button: (0,0,0,1)
+                        bg_color_stack_button: (0,0,0,1)
+                        label_text_color: (1,1,1,1)
+
+            MDBottomNavigationItem:
+                name: "screen 3"
+                text: "Progress"
+                icon: 'chart-bar'
+
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDLabel:
+                        text: 'Progress'
+                        text_size: self.size
+                        valign: 'top'
+                        halign: 'left'
+                        markup: True
+                        padding_x: '24dp'
+                        padding_y: '12dp'
+                        text_color: (0,0,0,1)
+                        font_style: 'H4'
+                        font_name: 'assets/fonts/Inter-SemiBold.ttf'
+
+                
                 
                 
 
@@ -52,28 +105,38 @@ Builder.load_string("""
         Button:
             text: 'My settings button'
         Button:
-            text: 'Back to menu'
-            on_press: root.manager.current = 'menu'
+            text: 'Back to home'
+            on_press: root.manager.current = 'home'
 """)
 
 # Declare both screens
-class MenuScreen(Screen):
-    pass
+class HomeScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        #Clock.schedule_once(self.set_toolbar_font_name)
+
+    def set_toolbar_font_name(self, *args):
+        self.ids.toolbar.ids.label_title.font_name = "assets/fonts/Inter-Medium.ttf"
 
 class SettingsScreen(Screen):
     pass
 
-class TestApp(MDApp):
+class App(MDApp):
 
+    data = {
+            'Recommendations' : 'star',
+            'Create workout' : 'lightning-bolt'
+        }
+    
     def build(self):
         # Create the screen manager
         sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(SettingsScreen(name='settings'))
-        # App Theme
+        
         
 
         return sm
 
 if __name__ == '__main__':
-    TestApp().run()
+    App().run()
