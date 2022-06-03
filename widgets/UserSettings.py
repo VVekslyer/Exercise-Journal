86fe0a71ca9c3dd8d742995649501a48e4926905
VVekslyer <Vekslyer.V@protonmail.com>
2022-06-03 15:45:20 -0700

    seperated screens and widgets

--- widgets/UserSettings.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class UserSettings(Screen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        self.orientation = 'vertical'
--- widgets/WhatAreYourGoals.py
+++
@@ -1,5 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class WhatAreYourGoals(Screen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
